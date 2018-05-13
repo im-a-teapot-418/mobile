a40bd1a67e2120308731397b6ea7b291370547a8
Nikola Jichev <njichev@gmail.com>
2018-05-13 13:09:11 +0300

    Add marker.

--- ignite/MobileScreens/ComponentActivitiesNearMe.js
+++ ignite/MobileScreens/ComponentActivitiesNearMe.js
@@ -5,19 +5,57 @@ import { Images } from './MobileTheme'
 import styles from './Styles/ComponentCheckInScreenStyles'
 import { MapView } from 'expo'
 
+import API from '../../App/Services/Api'
+
 class ComponentActivitiesNearMe extends React.Component {
+  api = {}
+
+  state = {
+    data: []
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.api = API.create()
+  }
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  componentWillMount() {
+    this.api.getFacilities().then((response) => {
+      this.setState({ data: response.data })
+    })
+  }
+
   render() {
     return (
-        <MapView
-          style={{ flex: 1 }}
-          onPress={(e) => this.props.navigation.goBack()}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+      <MapView
+        style={{ flex: 1 }}
+        onPress={(e) => this.props.navigation.goBack()}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      >
+        {this.state.data.map(marker => (
+          <MapView.Marker
+            coordinate={ { latitute: marker.latitude, longitude: marker.longitude } }
+            title={marker.name}
+          />
+        ))}
+      </MapView>
     );
   }
 }
